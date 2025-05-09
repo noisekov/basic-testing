cbbcfe5628e9ea86a96acba0ccc4122d76e8e1c3
noisekov <noise123q@mail.ru>
2025-05-09 11:30:09 +0300

    feat: finish simple-test

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -12,26 +12,62 @@ describe('simpleCalculator tests', () => {
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 13,
+        b: 3,
+        action: Action.Subtract,
+      }),
+    ).toBe(10);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 9,
+        b: 3,
+        action: Action.Multiply,
+      }),
+    ).toBe(27);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 9,
+        b: 3,
+        action: Action.Divide,
+      }),
+    ).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 3,
+        b: 3,
+        action: Action.Exponentiate,
+      }),
+    ).toBe(27);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 3,
+        b: 3,
+        action: 'invalid action',
+      }),
+    ).toBe(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: Symbol,
+        b: BigInt,
+        action: Action.Add,
+      }),
+    ).toBe(null);
   });
 });
