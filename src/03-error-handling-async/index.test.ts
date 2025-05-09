dd9e7662a80a73ae494d5e6d7e923828379a7c6d
noisekov <noise123q@mail.ru>
2025-05-09 16:06:25 +0300

    feat: solve all error-handling-async

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,9 +1,9 @@
 import {
   throwError,
-  // throwCustomError,
+  throwCustomError,
   resolveValue,
-  // MyAwesomeError,
-  // rejectCustomError,
+  MyAwesomeError,
+  rejectCustomError,
 } from './index';
 
 describe('resolveValue', () => {
@@ -15,22 +15,21 @@ describe('resolveValue', () => {
 describe('throwError', () => {
   test('should throw error with provided message', () => {
     expect(() => throwError('fuck')).toThrow('fuck');
-    expect(() => throwError()).toThrow('Oops!');
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    expect(() => throwError()).toThrow('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    expect(() => throwCustomError()).toThrow(MyAwesomeError);
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    await expect(() => rejectCustomError()).rejects.toThrow(MyAwesomeError);
   });
 });
