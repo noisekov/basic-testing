eaae7bdb08ff811ba725cfd6d01bf9bd8e22e675
noisekov <noise123q@mail.ru>
2025-11-03 15:53:36 +0300

    feat: write 06 chapter

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,5 +1,4 @@
-// Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -11,11 +10,15 @@ describe('doStuffByTimeout', () => {
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    doStuffByTimeout(() => {}, 1000);
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    doStuffByTimeout(callback, 1000);
+    expect(callback).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(1000);
+    expect(callback).toHaveBeenCalled();
   });
 });
 
@@ -29,24 +32,33 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    doStuffByInterval(() => {}, 1000);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    const callback = jest.fn();
+    doStuffByInterval(callback, 1000);
+    expect(callback).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(2000);
+    expect(callback).toHaveBeenCalledTimes(2);
   });
 });
 
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
-    // Write your test here
+    readFileAsynchronously('test');
   });
 
   test('should return null if file does not exist', async () => {
-    // Write your test here
+    expect(await readFileAsynchronously('test')).toBe(null);
   });
 
   test('should return file content if file exists', async () => {
-    // Write your test here
+    expect(
+      JSON.parse((await readFileAsynchronously('../../.prettierrc'))!),
+    ).toStrictEqual({
+      singleQuote: true,
+      trailingComma: 'all',
+    });
   });
 });
